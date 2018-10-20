9848658355ce968ba9d5a0c9b0bb0e86bb531142
Vijay NV <19.nvv.11@gmail.com>
2018-10-20 00:23:35 -0700

    small fixes

--- src/components/controls.js
+++ src/components/controls.js
@@ -4,50 +4,45 @@ import React from "react";
 //stylesheet
 import  '../../style/serendip.css';
 
-const sortcontrol = {
-    height: '70px',
-    borderBottom: 'solid 1px',
-    display: 'none'
-};
-
-const selectioncontrol = {
-    height: '100px',
-    borderBottom: 'solid 1px',
-};
-
-const show = {
-    display: "block"
-};
-
-const hide = {
-    display: "none"
-}
-
 class Controls extends Component{
     constructor(props){
         super(props);
         this.state={term:''};
         this.state.sortcontrol = "none";
-        this.state.selectioncontrol = hide;
-        this.expandsort = this.expandsort.bind(this);
+        this.state.selectioncontrol = "none";
+        this.togglesort = this.togglesort.bind(this);
+        this.toggleselection = this.toggleselection.bind(this);
     };
-    expandsort (){
-        this.state.sortcontrol= "block";
+    togglesort (){
+        var status = "block";
+        if(this.state.sortcontrol == "block"){
+            status = "none"
+        }
+        this.setState({sortcontrol:status});
+    };
+
+    toggleselection (){
+        var status = "block";
+        if(this.state.selectioncontrol == "block"){
+            status = "none"
+        }
+        this.setState({selectioncontrol:status});
     };
     render (){
         return (
             <div className="window side">
                 <div className="sidenavbar">Topic Controls</div>
                 <div>
-                    <div className="control" onClick={this.expandsort}>
+                    <div className="control" onClick={this.togglesort}>
                     Sorting
                     </div>
                     <div id='sortcontrol' style={{display: this.state.sortcontrol}}>
+                    <select style={{width: '150px'}}></select>
                     </div>
-                    <div className="control">
+                    <div className="control" onClick={this.toggleselection}>
                     Selection
                     </div>
-                    <div id='selectioncontrol' style={this.state.selectioncontrol}>
+                    <div id='selectioncontrol' style={{display: this.state.selectioncontrol}}>
                     </div>
                 </div>
             </div>
