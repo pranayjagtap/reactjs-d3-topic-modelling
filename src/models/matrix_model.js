70842ef6047ff33f6569f21751a3f7f0b7140f20
Vijay NV <19.nvv.11@gmail.com>
2018-11-03 11:47:29 -0700

    added row and column highlight in matrix view

--- src/models/matrix_model.js
+++ src/models/matrix_model.js
@@ -10,13 +10,13 @@ var x,xAxis;
 var y,yAxis;
 
 var dimensions = {
-    w_width: 2000,
-    w_height: 2000,
+    w_width: 1200,
+    w_height: 1200,
     margin: {
-        top: 80,
-        right: 80,
-        bottom: 80,
-        left: 180
+        top: 50,
+        right: 250,
+        bottom: 50,
+        left: 50
     },
     width: function(){
         return this.w_width - (this.margin.left + this.margin.right)
@@ -61,165 +61,26 @@ var read_data = function(){
     })
 };
 
-var init = function(){
-    x = d3.scale.ordinal()
-        .rangeRoundBands([0, dimensions.width()]);
-
-    y = d3.scale.ordinal()
-        .rangeRoundBands([dimensions.height(), 0]);
-
-    xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("top");
-
-    yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left")
-
-    matrix = d3.select("#matrix_canvas")
-        .attr("width", dimensions.w_width)
-        .attr("height", dimensions.w_height)
-        .append("g")
-        .attr("transform","translate("+dimensions.margin.left+","+dimensions.margin.top+")");
-
-    refresh();
-}
-
-var refresh = function(){
-
-    y.domain(matrix_data.map(function(d) { return d.Document; }));
-    x.domain(matrix_data.topicids);
-
-    //var allcols = Object.keys(matrix_data[0]), cols = allcols.slice(1,allcols.length);
-
-    // add x axis and tilt it by 45 degree and move it on top of the line
-    matrix.append("g")
-        .attr("class","x axis")
-        .attr("transform","translate(0,0)")
-        .call(xAxis)
-        .selectAll("text")
-        .style("text-anchor","start")
-        .attr("transform","rotate(-45)")
-        .attr("dx","-15px")
-        .attr("dy",x.rangeBand()/2 - 10)
-        .attr("lable_col", function(d, i){return "col_"+i});;
-
-    // add y axis and move it on top of the line
-    matrix.append("g")
-        .attr("class","y axis")
-        .attr("transform","translate(0,0)")
-        .call(yAxis)
-        .selectAll("text")
-        .attr("dx","0px")
-        .attr("dy",y.rangeBand()/2 + 10)
-        .attr("lable_row", function(d, i){return "row_"+i});
-
-    // add id to row, makes it easy to select it
-    var grows = matrix.selectAll(".grow")
-        .data(matrix_data)
-        .enter().append("g")
-        .attr("class","grow")
-        .attr("transform", function(d) { return "translate(0," + y(d.Document) + ")"; })
-        .attr("row_id", function(d, i) { return "row_"+i})
-    ;
-
-    var gcells = grows.selectAll(".gcell")
-        .data(function(d) { return d.groups; })
-        .enter() 
-        .append("g")
-        .attr("transform", function(d,i,j) {
-            return "translate(" + i*x.rangeBand() + ",0)" ; } )
-        .attr("class","gcell")
-        .attr("col_id", function(d, i){return "col_"+i})
-    ;
-
-    // add id to col, makes it easy to select it
-    // remove color of grid
-    gcells
-        .append("rect")
-        .attr("x",0)
-        .attr("y",0)
-        .attr("height",y.rangeBand())
-        .attr("width",x.rangeBand())
-        .style("stroke-opacity",0)
-        .style("stroke", "#ddd")
-    ;
-
-    var rmax = Math.min(y.rangeBand()/2-4,x.rangeBand()/2-4)
-    //6.5
-    gcells.append("circle")
-        .on('drag',()=>{})
-        .on('mouseover', circle_mouseover)
-        .on('mouseout', circle_mouseout)
-        .on('click',(d)=>{})
-        .attr("cy",y.rangeBand())
-        .attr("cx",x.rangeBand())
-        .attr("r", function(d) { return Math.abs((d.value));})  //Made radius value rind on 26-Oct-2018-Pranay
-        .style("fill", function(d) {return 'rgb(255,128,128)';})
-        .style("stroke","black")
-    ;
-
-}
-
-var circle_mouseover = function(doc) {
-    let colid = d3.select(this.parentNode).attr("col_id")
-    let rowid = d3.select(this.parentNode.parentNode).attr("row_id")
-    d3.select(this).transition()
-        .duration(100)
-        .style('stroke-width', 3)
-    d3.select("[row_id="+rowid+"]").selectAll("circle")
-        .style("fill", "rgb(255,64,64)")
-        .style("stroke-width", 3)
-    d3.selectAll("[col_id="+colid+"]").select("circle")
-        .style("fill", "rgb(255,64,64)")
-        .style("stroke-width", 3)
-    d3.select('[lable_row='+rowid+']')
-        .style("font-weight", 700)
-        .style("font-size", "12px")
-    d3.select('[lable_col='+colid+']')
-        .style("font-weight", 700)
-        .style("font-size", "12px")
-}
-
-var circle_mouseout = function(d) {
-    let colid = d3.select(this.parentNode).attr("col_id")
-    let rowid = d3.select(this.parentNode.parentNode).attr("row_id")
-    d3.select(this).transition()
-    .duration(100)
-    .style('stroke-width', 1);
-    d3.select("[row_id="+rowid+"]").selectAll("circle")
-        .style("fill", "rgb(255,128,128)")
-        .style("stroke-width", 1)
-    d3.selectAll("[col_id="+colid+"]").select("circle")
-        .style("fill", "rgb(255,128,128)")
-        .style("stroke-width", 1)
-    d3.select('[lable_row='+rowid+']')
-        .style("font-weight", 400)
-        .style("font-size", "10px")
-    d3.select('[lable_col='+colid+']')
-        .style("font-weight", 400)
-        .style("font-size", "10px")
-}
-
-export function test_matrix(){
-    var margin = {top: 50, right: 50, bottom: 50, left: 150},
-        width = 800 - margin.left - margin.right,
-        height = 800 - margin.top - margin.bottom;
+function test_matrix(){
+    d3.select("#matrix").remove();
+    // var margin = {top: 50, right: 50, bottom: 50, left: 150},
+    //     width = 1200 - margin.left - margin.right,
+    //     height = 1200 - margin.top - margin.bottom;
 
 
-    var y = d3.scale.ordinal().rangeBands([0, height],0,2),
-        x = d3.scale.ordinal().rangeBands([0, width],0,2);
+    var y = d3.scale.ordinal().rangeBands([0, dimensions.height()],0,2),
+        x = d3.scale.ordinal().rangeBands([0, dimensions.width()],0,2);
 
     var dragging = {};
 
 
     var svg = d3.select("#matrix_canvas")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .style("margin-left", margin.left + "px")
+        .attr("width", dimensions.w_width)
+        .attr("height", dimensions.w_height)
+        //.style("margin-left", dimensions.margin.left + "px")
         .append("g")
         .attr("id", "matrix")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("transform", "translate(" + (dimensions.margin.left+200) + "," + dimensions.margin.top + ")");
 
     // Maintain two matrix for doc view and topic view drag
     var top_matrix = [],doc_matrix = [];
@@ -246,14 +107,14 @@ export function test_matrix(){
         node.index = i;
         node.count = 0;
         top_matrix[i] = d3.range(d_cnt).map((j) => { 
-            return {x: j, y: i, z: matrix_data["weights"][j][i]}; });
+            return {x: j, y: i, z: scale_radius(matrix_data["weights"][j][i], 0, 10)}; });
     }); //50x36 matrix
 
     d_nodes.forEach(function(node, i) {
         node.index = i;
         node.count = 0;
         doc_matrix[i] = d3.range(t_cnt).map((j) => { 
-            return {x: i, y: j, z: matrix_data["weights"][i][j]}; });
+            return {x: i, y: j, z: scale_radius(matrix_data["weights"][i][j], 0, 10)}; });
     }); //36x50 matrix
 
     // Default order
@@ -290,6 +151,15 @@ export function test_matrix(){
             return "translate(" + x(i) + ")rotate(-90)"; })
         .each(column);
 
+    var gbox = svg.selectAll(".gbox")
+        .data(Array(50).fill().map(() => Array(36).fill(0)))
+        .enter().append("g")
+        .attr("class", "gbox")
+        .attr("col_id", function(d, i){return "grd_col_"+i;})
+        .attr("transform", function(d, i) { 
+            return "translate(" + (x(i)-10) + ", "+ (-15)+")"; })
+        .each(gbox);
+
     var trigger;
 
 
@@ -309,7 +179,7 @@ export function test_matrix(){
         .on("drag", function(d) {
             if (trigger == "label") {
                 d3.selectAll(".cellcolumn").attr("opacity", 0);
-                dragging[d[0].x] = Math.min(height, Math.max(0, d3.event.y));
+                dragging[d[0].x] = Math.min(dimensions.height(), Math.max(0, d3.event.y));
                 doc_orders.name.sort(function(a, b) { return position(a) - position(b); });
                 y.domain(doc_orders.name);
                 d3.selectAll(".row").attr("transform", function(d) { 
@@ -354,7 +224,7 @@ export function test_matrix(){
         .on("drag", function(d, i) {
             if (trigger == "label") {
                 d3.selectAll(".cellrow").attr("opacity", 0);
-                dragging[d[0].y] = Math.min(width, Math.max(0, d3.event.x));
+                dragging[d[0].y] = Math.min(dimensions.width(), Math.max(0, d3.event.x));
                 top_orders.name.sort(function(a, b) { return cPosition(a) - cPosition(b); });
                 x.domain(top_orders.name);
                 d3.selectAll(".column").attr("transform", function(d) { 
@@ -389,6 +259,7 @@ export function test_matrix(){
                 return d.z; 
             }))
             .enter().append("circle")
+            .attr("pos_id", function(d){return "pos_"+d.x+"_"+d.y;})
             .attr("class", "cellrow")
             .attr("cx", function(d) { 
                 if(x(d.y))
@@ -397,6 +268,18 @@ export function test_matrix(){
             .attr("fill", "#000")
             .style("r", function(d, i) { 
                 return d.z;
+            }).on("mouseover", function(d){
+                var pos_id = d3.select(this).attr("pos_id");
+                var row = "row_"+pos_id.split("_")[1];
+                var col = "col_"+pos_id.split("_")[2];
+                d3.select("[row_id="+row+"]").selectAll("circle").style("fill", "#fa0");
+                d3.select("[col_id="+col+"]").selectAll("circle").style("fill", "#fa0");
+            }).on("mouseout", function(d){
+                var pos_id = d3.select(this).attr("pos_id");
+                var row = "row_"+pos_id.split("_")[1];
+                var col = "col_"+pos_id.split("_")[2];
+                d3.select("[row_id="+row+"]").selectAll("circle").style("fill", "#000");
+                d3.select("[col_id="+col+"]").selectAll("circle").style("fill", "#000");
             });
     }
 
@@ -411,18 +294,47 @@ export function test_matrix(){
             .attr("cx", function(d) { 
                 return -y(d.x); 
             })
-            .attr("fill", "#f99")
+            .attr("fill", "#000")
             .style("r", function(d,i,j) { 
                 return d.z;
             })
             .on("mouseover", function(d){
-                var pos_id = d3.select(this).attr("pos_id");
-                var row = "row_"+pos_id.split("_")[2];
-                var col = "col_"+pos_id.split("_")[1];
-                // d3.select("[row_id="+row+"]").selectAll("circle").style("fill", "#5f5").style("opacity",1);
-                // d3.select("[col_id="+col+"]").selectAll("circle").style("fill", "#5f5").style("opacity",1);
+                
+            }).on("mouseout", function(d){
+                
             });
 
+    }
+
+    function gbox(gbox, j) {
+        var g = d3.select(this).selectAll(".gboxbox")
+            .data(gbox)
+            .enter().append("rect")
+            .attr("class", "gboxbox")
+            .attr("y", function(d, i){
+                return y(i);
+            })
+            .attr("row_id", function(d, i){
+                return "grd_row_"+i;
+            })
+            .attr("col_id", function(d, i){
+                return "grd_col_"+j;
+            })
+            .style("fill", "#aaa0")
+            .style("height", 28)
+            .style("width", 17)
+            .on("mouseout", function(d){
+                var rid = d3.select(this).attr("row_id").split("grd_row_")[1];
+                var cid = d3.select(this.parentNode).attr("col_id").split("grd_col_")[1]
+                d3.selectAll("[row_id=grd_row_"+rid+"]").style("fill", "#aaa0");
+                d3.selectAll("[col_id=grd_col_"+cid+"]").style("fill", "#aaa0");
+            })
+            .on("mouseover", function(d){
+                var rid = d3.select(this).attr("row_id").split("grd_row_")[1];
+                var cid = d3.select(this.parentNode).attr("col_id").split("grd_col_")[1]
+                d3.selectAll("[row_id=grd_row_"+rid+"]").style("fill", "#aaa5");
+                d3.selectAll("[col_id=grd_col_"+cid+"]").style("fill", "#aaa5");
+            });
     }
 
     d3.selection.prototype.moveToFront = function() {
@@ -446,8 +358,8 @@ export function test_matrix(){
     }
 }
 
-function new_matrix(){
-
+function scale_radius(r, min, max){
+    return (r-min)/(max-min) * 4;
 }
 export function render_matrix(){
     read_data();
